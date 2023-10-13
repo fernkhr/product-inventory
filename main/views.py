e9ac36caae930def41aa7ba32b6b0f08fc7fb1af
fernkhr <fern.khairunnisha@ui.ac.id>
2023-10-13 15:04:02 +0700

    commit

--- main/views.py
+++ main/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required(login_url='/login')
 # Create your views here.
@@ -100,3 +101,23 @@ def delete_item(request, id):
     item = Item.objects.get(pk = id)
     item.delete()
     return HttpResponseRedirect(reverse('main:show_main'))
+
+def get_item_json(request):
+    item_item = Item.objects.all()
+    return HttpResponse(serializers.serialize('json', item_item))
+
+@csrf_exempt
+def add_item_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        amount = request.POST.get("amount")
+        description = request.POST.get("description")
+        price = request.POST.get("price")
+        user = request.user
+
+        new_item = Item(name=name, amount=amount, description=description, price=price, user=user)
+        new_item.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
